b53a43b3095aae6dd204361c83c8a17a53be63b3
JereDAM <usr3593@salesianos-lacuesta.net>
2023-12-20 10:57:39 +0000

    Feat: Se creó el servicio de la api, que devolverá la actividad

--- screens/ActividadApiScreen.tsx
+++ screens/ActividadApiScreen.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import appColors from '../assets/styles/appColors'
 
 const ActividadApiScreen = () => {
+
+    let [boredActivity, setBoderActivity] = useState<string>()
+
   return (
-    <View>
-      <Text>ActividadApiScreen</Text>
-    </View>
+    <ImageBackground source={require("..\\assets\\images\\espacio.jpg")} resizeMode='cover' style={styles.backGround}>
+        <View>
+            <Text style={styles.activityTitle}>El HacerAlgoNator</Text>
+        </View>
+    </ImageBackground>
   )
 }
 
 export default ActividadApiScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    backGround: {
+        height: '100%'
+      },
+    activityTitle:{
+        color: appColors.primary,
+        fontSize: 40,
+        alignSelf: 'center',
+        marginTop: 20
+    }
+})
---
+++ services/boredService.ts
@@ -0,0 +1,44 @@
+import { Float } from "react-native/Libraries/Types/CodegenTypes";
+
+const BORED_API_URL = 'https://www.boredapi.com/api'
+const BORED_API_ENDPOINT = '/activity'
+
+type ActivitiesWhileBored = {
+    activity : string;
+    type: string;
+    participants: number;
+    price: Float;
+    link: string;
+    key: string;
+    accessibility: string
+}
+
+
+/**
+ * @param httpVerb Es la acción que va a tomar la consulta a la API (GET, POST, PUT, DELETE)
+ * @returns Devuelve el objeto que necesita
+ */
+
+const getInitRequest = (httpVerb: string): RequestInit => {
+    const init: RequestInit = {
+        method: httpVerb,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+      return init;
+}
+
+export const getActivity = async (TotalActivities : string): Promise<string> => {
+    let actividad: string =""
+
+    const request: RequestInfo = `${BORED_API_URL}${BORED_API_ENDPOINT}`
+    const response = await fetch(request, getInitRequest('GET'))
+    const json: ActivitiesWhileBored = await response.json()
+
+    if(json != null){
+        actividad = json.activity
+    }
+    return actividad
+}
